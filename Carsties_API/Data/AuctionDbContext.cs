e896f2c2af0dab40ec7b858f59a95f78095eb2f1
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-05 21:20:23 +0700

    Add => DbContext

---
+++ Carsties_API/Data/AuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Carsties_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carsties_API.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
