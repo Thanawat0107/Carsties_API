98354219ff3de932ef17d20b6678c6c8dfa6ea69
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-11 22:09:42 +0700

    Add => MongoDbConnection

--- Search_API/Program.cs
+++ Search_API/Program.cs
@@ -1,5 +1,7 @@
-using Search_API.Settings;
-using Search_API.Services;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Search_API.Data;
+using Search_API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,20 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 //SERVICES
-builder.Services.Configure<MongoDbSettings>(
-    builder.Configuration.GetSection("MongoDbSettings"));
-
-builder.Services.AddSingleton<ProductService>();
+//builder.Services.Configure<MongoDbSettings>(
+//    builder.Configuration.GetSection("MongoDbSettings"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -28,5 +31,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
